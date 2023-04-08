ba2156a18e36f092065286919ba070690f876da5
Zz_Pedro_Zz <80925383+pedro0102-hyand@users.noreply.github.com>
2023-04-08 10:58:52 -0300

    Create uri1042.py

---
+++ uri1042.py
@@ -0,0 +1,8 @@
+n1,n2,n3=map(int,input().split())
+originais=[n1,n2,n3]
+ordenado=sorted([n1,n2,n3])
+for valores in ordenado:
+    print(valores)
+print()
+for valores in originais:
+    print(valores)
